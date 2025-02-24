0e6fd5d77ffcbaa83f219b3162eba8e8a70ae2c1
didzislauva <didzis@lauvadidzis.com>
2025-02-24 21:04:47 +0200

    init

---
+++ blakus.php
@@ -0,0 +1,9 @@
+﻿<?php
+
+sleep(20);
+(float)$xcoord=$_POST['x'];
+(float)$ycoord=$_POST['y'];
+$id=$_POST['id'];
+(integer)$periodType=$_POST['periodType'];
+echo $xcoord." ".$ycoord." ".$id." ".$periodType;
+?>
